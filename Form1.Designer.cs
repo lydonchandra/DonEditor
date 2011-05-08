dda15b9c1783ffa27964c18c3874c8c411e1c59a
Lydon Chandra <lydonchandra@iphone.(none)>
2011-05-09 04:35:35 +0800

    can mosaic pics now

---
+++ Form1.Designer.cs
@@ -0,0 +1,114 @@
+﻿namespace DonEditor
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.menuStrip1 = new System.Windows.Forms.MenuStrip();
+            this.fileToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.openToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            this.mosaicToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.menuStrip1.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // menuStrip1
+            // 
+            this.menuStrip1.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.fileToolStripMenuItem});
+            this.menuStrip1.Location = new System.Drawing.Point(0, 0);
+            this.menuStrip1.Name = "menuStrip1";
+            this.menuStrip1.Size = new System.Drawing.Size(590, 24);
+            this.menuStrip1.TabIndex = 0;
+            this.menuStrip1.Text = "menuStrip1";
+            // 
+            // fileToolStripMenuItem
+            // 
+            this.fileToolStripMenuItem.DropDownItems.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.openToolStripMenuItem,
+            this.mosaicToolStripMenuItem});
+            this.fileToolStripMenuItem.Name = "fileToolStripMenuItem";
+            this.fileToolStripMenuItem.Size = new System.Drawing.Size(37, 20);
+            this.fileToolStripMenuItem.Text = "File";
+            // 
+            // openToolStripMenuItem
+            // 
+            this.openToolStripMenuItem.Name = "openToolStripMenuItem";
+            this.openToolStripMenuItem.Size = new System.Drawing.Size(152, 22);
+            this.openToolStripMenuItem.Text = "Open";
+            this.openToolStripMenuItem.Click += new System.EventHandler(this.openToolStripMenuItem_Click);
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.Location = new System.Drawing.Point(53, 27);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(483, 365);
+            this.pictureBox1.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.pictureBox1.TabIndex = 1;
+            this.pictureBox1.TabStop = false;
+            this.pictureBox1.MouseMove += new System.Windows.Forms.MouseEventHandler(this.pictureBox1_MouseMove);
+            this.pictureBox1.MouseDown += new System.Windows.Forms.MouseEventHandler(this.pictureBox1_MouseDown);
+            this.pictureBox1.MouseUp += new System.Windows.Forms.MouseEventHandler(this.pictureBox1_MouseUp);
+            // 
+            // mosaicToolStripMenuItem
+            // 
+            this.mosaicToolStripMenuItem.Name = "mosaicToolStripMenuItem";
+            this.mosaicToolStripMenuItem.Size = new System.Drawing.Size(152, 22);
+            this.mosaicToolStripMenuItem.Text = "Mosaic";
+            this.mosaicToolStripMenuItem.Click += new System.EventHandler(this.mosaicToolStripMenuItem_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(590, 428);
+            this.Controls.Add(this.pictureBox1);
+            this.Controls.Add(this.menuStrip1);
+            this.MainMenuStrip = this.menuStrip1;
+            this.Name = "Form1";
+            this.Text = "DonEditor";
+            this.Resize += new System.EventHandler(this.Form1_Resize);
+            this.MouseHover += new System.EventHandler(this.Form1_MouseHover);
+            this.menuStrip1.ResumeLayout(false);
+            this.menuStrip1.PerformLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.MenuStrip menuStrip1;
+        private System.Windows.Forms.ToolStripMenuItem fileToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem openToolStripMenuItem;
+        private System.Windows.Forms.PictureBox pictureBox1;
+        private System.Windows.Forms.ToolStripMenuItem mosaicToolStripMenuItem;
+    }
+}
+
---
+++ Form1.cs
@@ -0,0 +1,197 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Windows.Forms;
+using System.Diagnostics;
+using System.IO;
+using System.Diagnostics;
+
+namespace DonEditor
+{
+    public partial class Form1 : Form
+    {        
+        public Form1()
+        {
+            InitializeComponent();
+#if DEBUG
+            pictureBox1.Image = new Bitmap("../../don.tif");
+#endif
+            
+        }
+
+        protected override void OnPaint(PaintEventArgs e)
+        {
+            //Graphics g = e.Graphics;
+            //g.DrawImage(image,  ClientRectangle);
+        }
+
+        private void openToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+  
+            OpenFileDialog dialog = new OpenFileDialog(); 
+            dialog.Filter = "tif files (*.tif)|*.tif|All files (*.*)|*.*"; 
+            dialog.InitialDirectory = "."; 
+            dialog.Title = "Select a text file"; 
+            if ( dialog.ShowDialog() == DialogResult.OK ) {
+                System.GC.Collect();
+                pictureBox1.Image = new Bitmap(dialog.FileName);
+                this.Invalidate();
+            }
+
+        }
+
+        private void Form1_Resize(object sender, EventArgs e)
+        {
+            this.Invalidate();
+        }
+
+        private void Form1_MouseHover(object sender, EventArgs e)
+        {
+            //Cursor.Current = Cursors.Cross;
+        }
+
+        public Point startSelection = Point.Empty;
+        public Point endSelection = Point.Empty;
+        public Boolean isDrag = false;
+
+        
+
+        private void pictureBox1_MouseDown(object sender, MouseEventArgs e)
+        {
+            Cursor.Current = Cursors.Cross;
+
+            if (startSelection != Point.Empty &&
+                endSelection != Point.Empty)
+            {                
+                // undraw whatever previously drawn
+                DrawSelection(pictureBox1);
+            }
+
+            if (e.Button == MouseButtons.Left)
+            {
+                isDrag = true;
+                endSelection = Point.Empty;
+            }
+
+            startSelection = new Point(e.X, e.Y);                
+            
+        }
+
+        private void pictureBox1_MouseMove(object sender, MouseEventArgs e)
+        {
+            if (isDrag)
+            {
+                if (startSelection != Point.Empty &&
+                    endSelection != Point.Empty)
+                {                    
+                    // undraw
+                    DrawSelection(pictureBox1);
+                }
+                endSelection = new Point(e.X, e.Y);
+
+                DrawSelection(pictureBox1);
+
+
+            }            
+        }
+
+        public Rectangle DrawSelection(PictureBox pictureBox)
+        {
+            Size rectangleSize = new Size(endSelection.X - startSelection.X,
+                                          endSelection.Y - startSelection.Y);
+            
+            Rectangle rect = new Rectangle(startSelection, rectangleSize);
+            
+            ControlPaint.DrawReversibleFrame( pictureBox.RectangleToScreen(rect), Color.Red, FrameStyle.Thick);
+            
+            return rect;
+        }
+
+
+
+        private void pictureBox1_MouseUp(object sender, MouseEventArgs e)
+        {
+            if (isDrag)
+            {
+                //DrawSelection(pictureBox1);                
+                isDrag = false;
+            }            
+        }
+
+        private void mosaicToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            FolderBrowserDialog folderBrowserDialog = new FolderBrowserDialog();
+            folderBrowserDialog.Description = "Select directory that has your images";
+
+#if DEBUG
+            mosaicImages(@"d:\pics");
+#endif
+
+            if (folderBrowserDialog.ShowDialog() == DialogResult.OK)
+            {
+                Image bigImage = mosaicImages(folderBrowserDialog.SelectedPath);
+
+#if DEBUG                
+                bigImage.Save(@"c:\temp\bigImage.jpg");
+                return;
+#endif
+                bigImage.Save(folderBrowserDialog.SelectedPath+"\\..\\bigImage.jpg");
+            
+            }
+
+        }
+
+        private Image mosaicImages(String folderPath)
+        {
+            DirectoryInfo dirInfo = new DirectoryInfo(folderPath);
+            FileInfo[] fileInfos = dirInfo.GetFiles();
+            
+            int WIDTH = 20000;
+            int HEIGHT = WIDTH;
+
+            Image bigImage = new Bitmap( WIDTH, HEIGHT);
+            Graphics graphics = Graphics.FromImage(bigImage);
+
+            float x = 0.0F;
+            float y = 0.0F;
+
+            int maxHeight = 0;
+            
+            int columnWidth = 0;
+            foreach ( FileInfo fileInfo in fileInfos ) {
+                Debug.WriteLine(fileInfo.FullName);
+                try
+                {
+                    Image image = Image.FromFile(fileInfo.FullName);
+                    graphics.DrawImage(image, x, y);
+                    
+                    columnWidth += image.Width;
+                    int tempWidth = WIDTH - columnWidth;
+
+                    if (tempWidth < 0)
+                    {
+                        y = y + image.Height;
+                        x = 0;
+                        tempWidth = WIDTH;
+                        columnWidth = 0;
+                    }
+                    else
+                    {
+                        x = x + image.Width;
+                    }                    
+                    
+
+                } catch(Exception) {
+
+                }
+            }
+
+            return bigImage;
+        }
+        
+    }
+}
